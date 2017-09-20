d0bc15dd98ab475129b6b3a69f9ff3a8a80d2dc7
Kajan R <kajan.ravindran@gmail.com>
2017-09-20 14:11:44 -0400

    Commit 2
    Code up until the end of class before he pulled up his fall back

--- projectile.cpp
+++ projectile.cpp
@@ -1,3 +1,9 @@
 #include "projectile.hpp"
 
 #include <cstdio>
+
+const double m = 0;
+const double c = 1;
+
+auto force(TState s) {
+	return VecR2 {0,-m*g};}
--- projectile.hpp
+++ projectile.hpp
@@ -2,3 +2,26 @@
 
 #include <cstdio>
 
+struct VectR2 {
+	double x{0};
+	double y{0};
+}
+
+auto opperator+(VectR2 a, VectR2 b) {
+	return VectR2{a.x + b.x, a.y + b.y}
+}
+
+a + b;
+
+auto opperator*(VectR2 a, double c) { return VectR2{c * a.x, c * a.y}; }
+auto opperator*(double c, VectR2 a) { return a * c; }
+
+struct TState{
+	double t{0};
+	VectR2 postiion;
+	VectR2 velocity;
+};
+
+print_tstate(TState s) {
+	printf("%f %f %f %f %f\n", s.t, s.postiion.x, s.postiion.y, s.velocity.x, s.velocity.y);
+}
